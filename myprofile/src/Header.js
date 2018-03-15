3060549ab695a906d90835728dab856b37150160
Ning Zhang <ni.zhang@sap.com>
2018-03-15 20:24:32 +0800

    add my header.js

---
+++ myprofile/src/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Header extends React.Component{
+
+    render(){
+        return (<div>
+          <p>hello world</p>
+        </div>);
+    }; 
+}; 
