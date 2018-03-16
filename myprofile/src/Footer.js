c6cb9342bd4a3a9a8bb09575a330667755031d2d
Ning Zhang <ni.zhang@sap.com>
2018-03-16 15:19:47 +0800

    clean up

--- myprofile/src/Footer.js
+++ myprofile/src/Footer.js
@@ -1,16 +1,13 @@
 import React, {Component} from 'react'
 
 class Footer extends React.Component {
-
    render(){
        let sFooterContent = <div>
            <p>
                I am footer
            </p>
            </div>;
-
         return sFooterContent; 
-
    };
 
 }
